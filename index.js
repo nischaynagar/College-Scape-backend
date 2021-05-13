7159638b53d043b43eed2c9a86d3c5aa6b1e1299
nischaynagar <66030748+nischaynagar@users.noreply.github.com>
2021-05-14 00:33:57 +0530

    connected to myphp admin

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const mysql = require("mysql");
 const app = express();
 const port = 3000;
 
-const db = mysql.createPool({
+var db = mysql.createPool({
   host: "sql6.freesqldatabase.com",
-  user: "sql6409566",
-  password: "7fzVPL6NbF",
-  database: "sql6409566",
+  user: "sql6412158",
+  password: "B2wz1krpmX",
+  database: "sql6412158",
   port: "3306",
 });
 
@@ -21,30 +21,45 @@ app.get("/", (req, res) => {
   res.send("Welcome to collegeScape");
 });
 
-app.get("/api/auth", (req, res) => {
-  //For authentication of Admin
-  const name = req.query.AdminUserName;
-  const pass = req.query.AdminPass;
-  const sqlAuth = "SELECT * FROM admin WHERE AdminUserName=? ";
-  db.query(sqlAuth, [name], (err, result) => {
-    if (result.length > 0) {
-      if (pass === result[0].AdminPass) {
-        res.send({
-          result: 1,
-          username: result[0].AdminUserName,
-          name: result[0].AdminName,
-        });
-      } else {
-        res.send({
-          result: 2,
-        });
-      }
-    } else {
-      res.status(400);
-      res.send("3");
-    }
-  });
-});
+// db.connect((err) => {
+//   if (!err) {
+//     console.log("Connected");
+//   } else {
+//     console.log("Connection failed");
+//   }
+// });
+
+// app.get("/api/auth", (req, res) => {
+//For authentication of Admin
+
+// db.query(sqlAuth, [name], (err, result) => {
+//   if (result.length > 0) {
+//     if (pass === result[0].AdminPass) {
+//       res.send({
+//         result: 1,
+//         username: result[0].AdminUserName,
+//         name: result[0].AdminName,
+//       });
+//     } else {
+//       res.send({
+//         result: 2,
+//       });
+//     }
+//   } else {
+//     res.status(400);
+//     res.send("3");
+//   }
+// });
+// });
+
+// const UserName = "Nischay";
+// const name = "req.body.EmpName";
+// const pass = "req.body.EmpPass";
+// const type = "req.body.type";
+// const sqlInsert =
+//   "INSERT INTO employee(EmpUserName,EmpName,EmpPass,EmpType) VALUES (?,?,?,?);";
+// const sqlAuth = "SELECT * FROM employee WHERE EmpUserName=?";
+// db.query(sqlInsert, [UserName, name, pass, type], (error, result1) => {});
 
 app.listen(port, () => {
   console.log(`College Scape listening at http://localhost:${port}`);
